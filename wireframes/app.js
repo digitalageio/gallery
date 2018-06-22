75f12830838a60fcf3f7aaddeb9e6c86d401a395
Aaron Shannon <aaron@digitalage.io>
2018-06-22 00:26:20 -0400

    added several games to menu and tweaked wireframes

--- wireframes/app.js
+++ wireframes/app.js
@@ -1,4 +1,4 @@
-import {square, doubleSquare, cube, pyramid} from './models.js';
+import {tetrahedron, cube, octahedron} from './models.js';
 import {Camera} from './camera.js';
 import {toMesh} from './mesh.js';
 import {createRenderer} from './render.js';
@@ -8,13 +8,15 @@ const canvas = document.querySelector('canvas');
 const render = createRenderer(canvas);
 
 const scene = [
+    toMesh(tetrahedron),
     toMesh(cube),
-    toMesh(pyramid),
+    toMesh(octahedron)
 ];
 
 
 scene[0].color = "red";
 scene[1].color = "purple";
+scene[2].color = "green";
 
 const camera = new Camera();
 camera.pos.z = 200;
@@ -38,10 +40,16 @@ function animate(time){
         mesh.position.y = Math.sin(time / 7000) * 80;
         mesh.position.z = Math.cos(time / 5000) * 1000;
     }
+    {
+        const mesh = scene[2];
+        mesh.rotation.y -= 0.05;
+        mesh.position.x = Math.sin(time / 100) * 40;
+        mesh.position.y = Math.sin(time / 600) * 80;
+        mesh.position.z = Math.cos(time / 2000) * 100;
+    }
 
     render(scene, camera);
     requestAnimationFrame(animate);
 }
 
 animate(0);
-//drawMesh(mesh);
